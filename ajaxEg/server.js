e7638ffe57fafe721f85816c1378e33cf81fc3da
tejaswigowda <tejaswi@foxyninjastudios.com>
2022-01-31 11:24:49 -0700

    ac

--- ajaxEg/server.js
+++ ajaxEg/server.js
@@ -7,24 +7,28 @@ var hostname = process.env.HOSTNAME || 'localhost';
 var port = 1234;
 
 server.get("/add", function (req, res) {
+  console.log(req.query);
     var a = parseFloat(req.query.a);
     var b = parseFloat(req.query.b);
     res.send((a+b).toString()); // send response body
 });
 
 server.get("/sub", function (req, res) {
+  console.log(req.query);
     var a = parseFloat(req.query.a);
     var b = parseFloat(req.query.b);
     res.send((a-b).toString()); // send response body
 });
 
 server.get("/mul", function (req, res) {
+  console.log(req.query);
     var a = parseFloat(req.query.a);
     var b = parseFloat(req.query.b);
     res.send((a*b).toString()); // send response body
 });
 
 server.get("/div", function (req, res) {
+  console.log(req.query);
     var a = parseFloat(req.query.a);
     var b = parseFloat(req.query.b);
     res.send((a/b).toString()); // send response body
