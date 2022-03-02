c1aee1d94dd0881c0f56974c511aae1e22ed0557
tejaswigowda <tejaswi@foxyninjastudios.com>
2022-03-02 10:16:29 -0700

    ac

---
+++ ASSIGNMENTS/A5/README.md
@@ -0,0 +1,17 @@
+# Assignment 5
+`Due Mar 14, before class`
+
+## Task
+
+Please complete the example code (in `code` folder) so as to 
+implement the `Delete Feed` functionality. Refer to class code
+`rssReader`.
+
+
+## Usage (`code` folder)
+
+Make sure your `mongod` is running.
+
+$ `node server.js`
+
+go to <http://127.0.0.1:1234> in your browser.
---
+++ ASSIGNMENTS/A5/rssReader/server.js
@@ -0,0 +1,62 @@
+var express = require("express");
+var server = express();
+var bodyParser = require('body-parser');
+var errorHandler = require('errorhandler');
+var methodOverride = require('method-override');
+var hostname = process.env.HOSTNAME || 'localhost';
+var port = 1234;
+var Client = require('node-rest-client').Client;
+var client = new Client();
+var MS = require("mongoskin");
+
+var db = MS.db("mongodb://127.0.0.1:27017/rssReader")
+ 
+
+server.get("/addFeed", function (req, res) {
+  var url = req.query.url;
+  var obj = {
+    time: new Date().getTime(),
+    url: url,
+    id: new Date().getTime().toString(),
+    name: "Untitled"
+  }
+
+  db.collection("data").insert(obj, function(e,r){
+    res.send("1");
+  });
+});
+
+server.get("/editFeed", function (req, res) {
+  var id = req.query.id;
+  var newName = req.query.newName;
+  db.collection("data").findOne({id:id}, function(e,r){
+    console.log(r);
+    r.name = newName;
+    db.collection("data").save(r, function(e1,r1){
+      res.send("1");
+    });
+  });
+});
+
+server.get("/getAllFeeds", function (req, res) {
+  db.collection("data").find({}).toArray(function(e,r){
+    res.send(JSON.stringify(r))
+  });
+});
+
+server.get("/makeHTTPReq", function (req, res) {
+  var url = req.query.url;
+  client.get(url, function (data, response) {
+    // parsed response body as js object
+    console.log(data);
+    res.send(data);
+  });
+});
+
+server.use(methodOverride());
+server.use(bodyParser());
+server.use(errorHandler());
+server.use(express.static(__dirname + '/public'));
+
+console.log("Simple static server listening at http://" + hostname + ":" + port);
+server.listen(port);
