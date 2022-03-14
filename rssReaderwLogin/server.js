074972bc05ac64e7424b9222871f2cdb094adf65
tejaswigowda <tejaswi@foxyninjastudios.com>
2022-03-14 11:22:22 -0700

    ac

--- rssReaderwLogin/server.js
+++ rssReaderwLogin/server.js
@@ -1,18 +1,43 @@
-var express = require("express");
-var server = express();
-var bodyParser = require('body-parser');
-var errorHandler = require('errorhandler');
-var methodOverride = require('method-override');
-var hostname = process.env.HOSTNAME || 'localhost';
-var port = 1234;
-var Client = require('node-rest-client').Client;
-var client = new Client();
-var MS = require("mongoskin");
+var url = require("url"),
+	querystring = require("querystring");
+var passport = require('passport');
+var fs = require('fs');
+	var dbURL = 'mongodb://127.0.0.1:27017/rssReader';
+var path = require('path'),
+  express = require('express'),
+  db = require('mongoskin').db(dbURL);
 
-var db = MS.db("mongodb://127.0.0.1:27017/rssReader")
- 
 
-server.get("/addFeed", function (req, res) {
+var mongoose = require('mongoose');
+mongoose.connect(dbURL); // connect to our database
+
+var app = express();
+var secret = 'test' + new Date().getTime().toString()
+
+var session = require('express-session');
+app.use(require("cookie-parser")(secret));
+var MongoStore = require('connect-mongo')(session);
+app.use(session( {store: new MongoStore({
+   url: dbURL,
+   secret: secret
+})}));
+app.use(passport.initialize());
+app.use(passport.session());
+var flash = require('express-flash');
+app.use( flash() );
+
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+
+app.use(methodOverride());
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended:false
+}));
+require('./passport/config/passport')(passport); // pass passport for configuration
+require('./passport/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+app.get("/addFeed", function (req, res) {
   var url = req.query.url;
   var obj = {
     time: new Date().getTime(),
@@ -26,7 +51,7 @@ server.get("/addFeed", function (req, res) {
   });
 });
 
-server.get("/editFeed", function (req, res) {
+app.get("/editFeed", function (req, res) {
   var id = req.query.id;
   var newName = req.query.newName;
   db.collection("data").findOne({id:id}, function(e,r){
@@ -38,13 +63,13 @@ server.get("/editFeed", function (req, res) {
   });
 });
 
-server.get("/getAllFeeds", function (req, res) {
+app.get("/getAllFeeds", function (req, res) {
   db.collection("data").find({}).toArray(function(e,r){
     res.send(JSON.stringify(r))
   });
 });
 
-server.get("/makeHTTPReq", function (req, res) {
+app.get("/makeHTTPReq", function (req, res) {
   var url = req.query.url;
   client.get(url, function (data, response) {
     // parsed response body as js object
@@ -53,10 +78,17 @@ server.get("/makeHTTPReq", function (req, res) {
   });
 });
 
-server.use(methodOverride());
-server.use(bodyParser());
-server.use(errorHandler());
-server.use(express.static(__dirname + '/public'));
 
-console.log("Simple static server listening at http://" + hostname + ":" + port);
-server.listen(port);
+app.use(express.static(path.join(__dirname, 'public')));
+app.listen(8080);
+
+console.log("server running at http://localhost:8080")
+
+// route middleware to ensure user is logged in
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated())
+        return next();
+
+    res.send('noauth');
+}
+
